23c597cbb20663c59d901035e8a27861bce3e2ab
Phil <philrsheridan@gmail.com>
2023-11-11 23:30:56 -0500

    passing 14 tests, stuck adding hour to createTimeInEvent

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function createEmployeeRecord (employeeArray) {
+    const employee = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee;
+}
+
+function createEmployeeRecords (employeesArray) {
+    const employeeList = [];
+    for (let employee in employeesArray) {
+        employeeList.push(createEmployeeRecord(employeesArray[employee]));
+    };
+    return employeeList;
+};
+
+function createTimeInEvent (employee, dt) {
+    const tempDate = dt.slice(0, 10);
+    const tempTime = dt.slice(11, 15);
+
+    console.log(tempTime)
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: tempTime,
+        date: tempDate
+    });
+    return employee;
+};
